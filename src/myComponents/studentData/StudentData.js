7b226015fa60ba6bc5e575dc1b70b7a3e8c5f735
naeemali321123 <naeemali321123@gmail.com>
2022-03-30 18:29:42 +0500

    studentdata

--- src/myComponents/studentData/StudentData.js
+++ src/myComponents/studentData/StudentData.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import {useStyle}  from './StudentDataStyle'
-
+import { Hd } from "@material-ui/icons";
+import React, { useState } from "react";
+import { useStyle } from "./StudentDataStyle";
 
 export default function StudentData() {
-   let classes = useStyle
+  const [id, setId] = useState();
+  const [userName, setUserName] = useState();
+  const [email, setEmail] = useState();
+  let classes = useStyle;
   return (
-    <div style={{display: 'flex', padding: '15px'}}>
-        <div style={classes.button}>Name</div>
-        <div style={classes.button}>Email</div>
-        <div style={classes.button}>Roll#</div>
-    </div>
-  )
+    <>
+      <div style={{ display: "flex", padding: "15px" }}>
+        <table style={classes.border}>
+          <tr>
+            <th style={classes.border}>ID</th>
+            <th style={classes.border}>UserName</th>
+            <th style={classes.border}>Email</th>
+          </tr>
+          <tr>
+            <td style={classes.border}><input type="text" placeholder=" ID" onChange={(e)=>{setId(e.target.value)}} value={userName}/></td>
+            <td style={classes.border}><input type="text" placeholder=" User Name" onChange={(e)=>{setUserName(e.target.value)}} value={userName}/></td>
+            <td style={classes.border}><input type="text" placeholder=" Email" onChange={(e)=>{setEmail(e.target.value)}} value={userName}/></td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
 }
--- src/myComponents/studentData/StudentDataStyle.js
+++ src/myComponents/studentData/StudentDataStyle.js
@@ -1,9 +1,17 @@
 export const useStyle = {
-  button: {
+  heading: {
     padding: "15px",
     backgroundColor: "purple",
     color: "white",
     border: "2px solid black",
     borderRadius: "10%",
   },
+  student: {
+    padding: "15px",
+    border: "2px solid black",
+    borderRadius: "10%",
+  },
+  border: {
+    border: '2px solid black'
+  }
 };
